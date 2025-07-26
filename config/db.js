dc445dfdf90c703cdc5854f445d2bd52e64a8aa8
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-07-26 18:12:53 +0545

    db.js

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/db_propbox_system");
+        console.log("MongoDb Connected")
+    } catch (e) {
+        console.log("MongoDb not Connected");
+    }
+}
+
+module.exports=connectDB;
