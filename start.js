c517ea11f40c07fd222c17d5dded51ba29146d58
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-07-26 18:13:03 +0545

    start.js

---
+++ start.js
@@ -0,0 +1,8 @@
+// start.js
+require('dotenv').config();
+const app = require('./app');
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
